e6e2f175a21a58859a2cc99e19e70df1f2318239
pylagg <payal.aggarwal@nagarro.com>
2020-03-26 18:02:13 +0530

    hey

--- Hey.java
+++ Hey.java
@@ -1,7 +1,9 @@
 
 public class Hey {
-public static void main(String args[])
-{
-	System.out.println("Hey World");
-}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hey World!");
+	}
+
 }
